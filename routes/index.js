49586a762d4b82fa2e90b1a5042792c0f4211e58
Pauline Rouvel <pauline.rouvel@hotmail.fr>
2019-06-26 21:16:25 +0200

    add cors for requests

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 'use strict';
 
+const cors = require('cors');
+
+
 const database = require('../models/database');
 class RouterBuilder {
    build(app) {
+      
+      app.use(cors());
+
+        // Add headers
+        app.use(function (req, res, next) {
+
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', '*');
+
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,X-Requested-With,content-type, x-request-id');
+
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', true);
+
+            // Pass to next layer of middleware
+            next();
+        });
      app.use('/product', require('./produit_route')),
      app.use('/user', require('./utilisateur_route')),
      app.use('/order', require('./commande_route')),
