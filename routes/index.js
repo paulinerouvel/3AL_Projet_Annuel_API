4c307cbff1b6e96570386aa6801a6410f66659e5
pauline.rouvel <pauline.rouvel@hotmail.fr>
2019-06-27 18:18:53 +0200

    add cors for requests

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,40 @@
 'use strict';
 
-const database = require('../models/database');
+const cors = require('cors');
+
 class RouterBuilder {
-   build(app) {
-     app.use('/product', require('./produit_route')),
-     app.use('/user', require('./utilisateur_route')),
-     app.use('/order', require('./commande_route')),
-     app.use('/alert', require('./alerte_route')),
-     app.use('/key', require('./motCle_route')),
-     app.use('/donation', require('./don_route')),
-     app.use('/warehouse', require('./entrepot_route'))
-    }
+  build(app) {
+
+    app.use(cors());
+
+    // Add headers
+    app.use(function (req, res, next) {
+
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', '*');
+
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,X-Requested-With,content-type, x-request-id');
+
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+
+      // Pass to next layer of middleware
+      next();
+    });
+
+    app.use('/product', require('./produit_route')),
+    app.use('/user', require('./utilisateur_route')),
+    app.use('/order', require('./commande_route')),
+    app.use('/alert', require('./alerte_route')),
+    app.use('/key', require('./motCle_route')),
+    app.use('/donation', require('./don_route')),
+    app.use('/warehouse', require('./entrepot_route'))
+  }
 }
 
 module.exports = new RouterBuilder();
