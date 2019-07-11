e5517393a5cbb54fe133b614bf2473dad39c149a
Grégory <gregory.brugnet@gmail.com>
2019-07-11 22:52:41 +0200

    added getAllProductsByUserCategory(userCategoryID)

--- controllers/produit_controller.js
+++ controllers/produit_controller.js
@@ -129,6 +129,17 @@ class ProduitController {
 
     }
 
+    async getAllProductsByUserCategory(userCategoryID) {
+        try {
+            const res = await Database.connection.query('SELECT * FROM liste_produit JOIN `utilisateur_has_categorie` ON liste_produit.Utilisateur_id = utilisateur_has_categorie.Utilisateur_id WHERE utilisateur_has_categorie.Categorie_utilisateur_id = 1');
+            return res[0].map((rows) => new CategorieProduit(rows[0].id, rows[0].libelle));
+        }
+        catch (err) {
+            console.log(err);
+            return undefined;
+        }
+    }
+
     async getProductByCategorieAndDest(idCategorie, dest) {
         try {
             const res = await Database.connection.query('SELECT * FROM `produit` WHERE CategorieProduit_id = ? AND destinataire = ? AND enRayon = 1', [idCategorie, dest]);
