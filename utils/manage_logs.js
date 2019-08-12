abc922926d1df3c8eb0a1fb1a1ef4678f325b2d9
pauline.rouvel <pauline.rouvel@hotmail.fr>
2019-08-12 14:40:54 +0200

    manage logs with file in the server

--- utils/manage_logs.js
+++ utils/manage_logs.js
@@ -1,14 +1,37 @@
 const fs = require('fs');
 
+const form_data = require('form-data');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const Blob = require('blob');
+
+const request = require('request')
+
+
 module.exports = {
     generateLogs(err, fileName, functionName){
+
 
         let d = new Date(Date.now()).toLocaleString();
         let errScript = "<" + d + ">    API - " + fileName + " at " + functionName + "   " + err + "\n";
 
-
         fs.appendFile("wm_api_logs.log", errScript, function(err){
             if(err) throw err
         });
+
+
+        var req = request.post("http://51.75.143.205:8080/logs/api", function (err, resp, body) {
+            if (err) {
+              console.log('Error!', err);
+            } else {
+              console.log('URL: ' + body);
+            }
+          });
+
+          var form = req.form();
+
+          form.append('file', fs.createReadStream("wm_api_logs.log"));
+
+
+        req;
     }
 }
