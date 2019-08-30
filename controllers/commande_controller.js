ecea1c7f7a13401b26c38d74d8ffda1b6bceb293
pauline.rouvel <pauline.rouvel@hotmail.fr>
2019-08-30 14:57:42 +0200

    date bug

--- controllers/commande_controller.js
+++ controllers/commande_controller.js
@@ -78,7 +78,7 @@ class CommandeController {
         console.log("commande", cmd);
 
         let now = new Date(Date.now());
-        let dateT = now.toLocaleString('fr-FR').split(' ');
+        let dateT = now.split('T');
         let date = dateT[0].split('-');
 
         console.log(now);
